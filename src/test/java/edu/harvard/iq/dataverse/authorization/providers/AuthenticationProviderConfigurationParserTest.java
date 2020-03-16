8ba09ca7132704d54139e6f9483537ea719a0dcc
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-03-16 18:25:14 +0100

    Add tests for validation of AuthenticationProviderConfiguration. #6694

--- src/test/java/edu/harvard/iq/dataverse/authorization/providers/AuthenticationProviderConfigurationParserTest.java
+++ src/test/java/edu/harvard/iq/dataverse/authorization/providers/AuthenticationProviderConfigurationParserTest.java
@@ -1,5 +1,6 @@
 package edu.harvard.iq.dataverse.authorization.providers;
 
+import edu.harvard.iq.dataverse.authorization.exceptions.AuthenticationProviderConfigurationException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -37,5 +38,28 @@ class AuthenticationProviderConfigurationParserTest {
         AuthenticationProviderConfiguration config = AuthenticationProviderConfigurationParser.parse(content);
         
         assertNotNull(config);
+    }
+    
+    @Test
+    void testValidatorLoading() throws Exception {
+        assertDoesNotThrow(() -> AuthenticationProviderConfigurationParser.getValidator());
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"minimal-valid.json", "title-i18n-valid.json", "orcid-valid.json", "shib-valid.json"})
+    void validateValid(String filename) throws Exception {
+        Path file = Paths.get(ClassLoader.getSystemResource("auth-providers/"+filename).toURI());
+        String content = new String(Files.readAllBytes(file), StandardCharsets.UTF_8);
+        
+        assertTrue(AuthenticationProviderConfigurationParser.validate(content));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"minimal-invalid.json"})
+    void validateInvalid(String filename) throws Exception {
+        Path file = Paths.get(ClassLoader.getSystemResource("auth-providers/"+filename).toURI());
+        String content = new String(Files.readAllBytes(file), StandardCharsets.UTF_8);
+        
+        assertThrows(AuthenticationProviderConfigurationException.class, () -> AuthenticationProviderConfigurationParser.validate(content));
     }
 }
