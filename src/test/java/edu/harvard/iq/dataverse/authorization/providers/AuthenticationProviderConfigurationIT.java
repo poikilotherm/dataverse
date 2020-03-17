d665f699f170e52e3068ea7bda1158db9d0f103d
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-03-17 01:26:24 +0100

    Introduce basic testcontainers.org based persistance integration testing. Found the missing converter annotation.

---
+++ src/test/java/edu/harvard/iq/dataverse/authorization/providers/AuthenticationProviderConfigurationIT.java
@@ -0,0 +1,59 @@
+package edu.harvard.iq.dataverse.authorization.providers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testcontainers
+class AuthenticationProviderConfigurationIT {
+    
+    @Container
+    PostgreSQLContainer sql = new PostgreSQLContainer("postgres:9.6")
+                                      .withDatabaseName("dataverse")
+                                      .withUsername("dataverse")
+                                      .withPassword("dataverse");
+    
+    EntityManager em;
+    EntityTransaction tx;
+    
+    @BeforeEach
+    public void init() {
+        Map<String, String> configuration = new HashMap<>();
+        configuration.put("javax.persistence.jdbc.url", sql.getJdbcUrl());
+        configuration.put("javax.persistence.jdbc.user", sql.getUsername());
+        configuration.put("javax.persistence.jdbc.password", sql.getPassword());
+        configuration.put("javax.persistence.jdbc.driver", sql.getDriverClassName());
+        this.em = Persistence.
+            createEntityManagerFactory("integration-test", configuration).
+            createEntityManager();
+        this.tx = this.em.getTransaction();
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"minimal-valid.json", "title-i18n-valid.json"})
+    void testPersistance(String filename) throws Exception {
+        Path file = Paths.get(ClassLoader.getSystemResource("auth-providers/"+filename).toURI());
+        String content = new String(Files.readAllBytes(file), StandardCharsets.UTF_8);
+        
+        AuthenticationProviderConfiguration config = AuthenticationProviderConfigurationParser.parse(content);
+        
+        em.persist(config);
+    }
+}
