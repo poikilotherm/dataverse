cc0dfee5a4bfcb5ef30bc4c6a63ce26b9a17ade9
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-12-03 19:38:41 +0100

    Add unit test for caching of MailSessionProducer. #7424

--- src/test/java/edu/harvard/iq/dataverse/util/MailSessionProducerTest.java
+++ src/test/java/edu/harvard/iq/dataverse/util/MailSessionProducerTest.java
@@ -1,14 +1,16 @@
 package edu.harvard.iq.dataverse.util;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.mail.Session;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MailSessionProducerTest {
     
-    @BeforeAll
-    static void setUp() {
+    @BeforeEach
+    void setUp() {
         System.setProperty(MailSessionProducer.MAIL_CONFIG_PREFIX+".test", "test");
     }
     
@@ -17,5 +19,34 @@ class MailSessionProducerTest {
         MailSessionProducer test = new MailSessionProducer();
         assertEquals(1, test.getMailProperties().stringPropertyNames().size());
         assertTrue(test.getMailProperties().stringPropertyNames().contains("mail.smtp.test"));
+    }
+    
+    @Test
+    void testSessionCacheHits() {
+        // given
+        MailSessionProducer test = new MailSessionProducer();
+        Session testSession = test.getSystemMailSession();
+        
+        //when
+        assertEquals("test", testSession.getProperty("mail.smtp.test"));
+        testSession = test.getSystemMailSession();
+        
+        // then
+        assertEquals("test", testSession.getProperty("mail.smtp.test"));
+    }
+    
+    @Test
+    void testSessionCacheUpdates() {
+        // given
+        MailSessionProducer test = new MailSessionProducer();
+        Session testSession = test.getSystemMailSession();
+        
+        //when
+        assertEquals("test", testSession.getProperty("mail.smtp.test"));
+        System.setProperty(MailSessionProducer.MAIL_CONFIG_PREFIX+".test", "test2");
+        testSession = test.getSystemMailSession();
+        
+        // then
+        assertEquals("test2", testSession.getProperty("mail.smtp.test"));
     }
 }
